95f9de13d2962ebed90d89764f392b035fa5990d
Caelan <caelan.curry@gmail.com>
2022-01-23 20:20:36 +0200

    Added chroma-js functionality

--- src/Palette.js
+++ src/Palette.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
+import { generatePalette } from "./colorHelpers";
+import seedColors from "./seedColors";
 import "./Palette.css";
 
 export default class Palette extends Component {
 	render () {
-		const colorBoxes = this.props.colors.map((color) => <ColorBox background={color.color} name={color.name} />);
+		console.log(generatePalette(seedColors[4]));
+
+		const colorBoxes = this.props.colors.map((color, i) => (
+			<ColorBox key={color.name} background={color.color} name={color.name} />
+		));
+
 		return (
 			<div className="Palette">
 				<div className="Palette-colors">{colorBoxes}</div>
