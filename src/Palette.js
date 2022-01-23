1652f8dcfe6e3156fba57d3a10153303c299de15
Caelan <caelan.curry@gmail.com>
2022-01-23 21:58:32 +0200

    Added slider for color lightness

--- src/Palette.js
+++ src/Palette.js
@@ -1,19 +1,42 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
-import { generatePalette } from "./colorHelpers";
-import seedColors from "./seedColors";
+import Slider, { Range } from "rc-slider";
+import "rc-slider/assets/index.css";
 import "./Palette.css";
 
 export default class Palette extends Component {
-	render () {
-		console.log(generatePalette(seedColors[4]));
+	constructor (props) {
+		super(props);
+		this.state = { level: 500 };
+		this.changeLevel = this.changeLevel.bind(this);
+	}
 
-		const colorBoxes = this.props.colors.map((color, i) => (
-			<ColorBox key={color.name} background={color.color} name={color.name} />
+	changeLevel (level) {
+		this.setState({ level });
+	}
+
+	render () {
+		const { colors } = this.props.palette;
+		const { level } = this.state;
+
+		const colorBoxes = colors[level].map((color) => (
+			<ColorBox key={color.name} background={color.hex} name={color.name} />
 		));
 
 		return (
 			<div className="Palette">
+				<div className="slider-container">
+					<span>Level: {level}</span>
+					<Slider
+						className="slider"
+						defaultValue={level}
+						min={100}
+						max={900}
+						step={100}
+						onAfterChange={this.changeLevel}
+					/>
+				</div>
+
 				<div className="Palette-colors">{colorBoxes}</div>
 			</div>
 		);
