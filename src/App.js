3b0b275eeffa8b016e3d0535611b7102b6d9a335
Caelan <caelan.curry@gmail.com>
2022-01-24 18:10:27 +0200

    Added router and hompage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import Palette from "./Palette";
 import { generatePalette } from "./colorHelpers";
 import seedColors from "./seedColors";
+import { Route, Switch } from "react-router-dom";
 
 function App () {
+	function findPalette (id) {
+		return seedColors.find(function (palette) {
+			return palette.id === id;
+		});
+	}
+
 	return (
-		<div>
-			<Palette palette={generatePalette(seedColors[3])} />
-		</div>
+		<Switch>
+			<Route exact path="/" render={() => <h1>Palette list goes here</h1>} />
+			<Route
+				exact
+				path="/palette/:id"
+				render={(routeProps) => <Palette palette={generatePalette(findPalette(routeProps.match.params.id))} />}
+			/>
+		</Switch>
 	);
 }
 
