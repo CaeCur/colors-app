d6aeb97d009e6be43f9dde2c49d8ac0024f06668
Caelan <caelan.curry@gmail.com>
2022-01-24 14:26:28 +0200

    Added MaterialUI and color format selector

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
 
 export default class Navbar extends Component {
+	constructor (props) {
+		super(props);
+		this.state = { format: "hex" };
+		this.changeFormat = this.changeFormat.bind(this);
+	}
+
+	changeFormat (evt) {
+		this.setState({ format: evt.target.value }, () => {
+			this.props.changeFormat(this.state.format);
+		});
+	}
+
 	render () {
 		const { level, changeLevel } = this.props;
+		const { format } = this.state;
 
 		return (
 			<nav className="Navbar">
@@ -23,6 +38,14 @@ export default class Navbar extends Component {
 						step={100}
 						onAfterChange={changeLevel}
 					/>
+				</div>
+
+				<div className="select-container">
+					<Select value={format} onChange={this.changeFormat}>
+						<MenuItem value="hex">Hex</MenuItem>
+						<MenuItem value="rgb">RGB</MenuItem>
+						<MenuItem value="rgba">RGBA</MenuItem>
+					</Select>
 				</div>
 			</nav>
 		);
