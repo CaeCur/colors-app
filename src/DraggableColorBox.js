dcb7e500b7aff50ebfc89c72578a6968fe9a7f4d
Caelan <caelan.curry@gmail.com>
2022-01-27 19:18:06 +0200

    Added draggable color box comp

---
+++ src/DraggableColorBox.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+const styles = {
+	root : {
+		width        : "20%",
+		height       : "25%",
+		margin       : "0 auto",
+		display      : "inline-block",
+		position     : "relative",
+		cursor       : "grab",
+		marginBottom : "-4px"
+	}
+};
+
+export default function DraggableColorBox (props) {
+	return (
+		<Box component="div" sx={styles.root} style={{ backgroundColor: props.color }}>
+			{props.color}
+		</Box>
+	);
+}
